ff92b62faded0f78ea68fd44534bc6ee1dffe061
devopschandra369 <devops.chandra369@gmail.com>
2023-03-19 12:45:45 +0530

    added hellowq line in master

--- DBUtils.java
+++ DBUtils.java
@@ -1,9 +1,8 @@
 GIT SSH - demo
 DBUtils.java - GIT CHerry-pick demo
-
 GIT STASH Demo - 1st time
-
 Hello , This line is Updating from --> MASTER BRANCH 
 This line is updated from --> DEVELOPMENT BRANCH
 This file contains the database related code. 
+Hello
 Having some utilities for connection. 
