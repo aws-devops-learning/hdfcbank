b4a59f009673d038cd948fa4ff7b9177661e2d24
devopschandra369 <devops.chandra369@gmail.com>
2023-03-19 09:42:46 +0530

    Merged in master

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 Updated from MASTER BRANCH - fro GIT stash Demo
 This line is added  2nd time  for GIT STASH demo -
 Deploy.sh - GIT CHerry-pick demo
+=======
+Deploy.sh - GIT CHerry-pick demo
+
+>>>>>>> 8c8e59d (Deploy.sh - GIT CHerry-pick demo)
 Added this line form  -- DEV Branch
 
 This file contains deployment script.
